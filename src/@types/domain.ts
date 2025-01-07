7c1cdb31a5a700e79c7dc8cd3c84996633f4912d
visvaden7 <visvaden7@naver.com>
2025-01-07 15:14:01 +0900

    feat: add portfolio type and option type

--- src/@types/domain.ts
+++ src/@types/domain.ts
@@ -5,9 +5,25 @@ export type Project = {
   imgUrl: string;
   tag: '카드뉴스' | '기본';
 }
+
+export type Portfolio = {
+  idx: number,
+  imgUrl: string,
+  title: string,
+  subtitle: string,
+  user: string,
+  timestamp: string,
+  heartRate: number
+}
+
 export type PageReducerType = {
   type: string;
   maxPage?: number;
-  itemsPerPage?: number;
   page?: number
+}
+
+export type Options = {
+  id: number;
+  value: 'latest' | 'popular';
+  label: string;
 }
