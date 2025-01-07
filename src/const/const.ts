a952e1523334e24733aba2e7c99d566cde0ddcef
visvaden7 <visvaden7@naver.com>
2025-01-07 15:13:13 +0900

    chore: add dummy data

--- src/const/const.ts
+++ src/const/const.ts
@@ -1,84 +1,171 @@
 import {Project} from "../@types/domain.ts";
 
-// const portfolioList = [
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   },
-//   {
-//     imgUrl: 'https://placehold.co/150',
-//     projectName: '강남구 미래인재 스마트시티',
-//     portFolioNmae: '안전한 나의 친구',
-//     user: 'ahnji1',
-//     timestamp: '24.10.25'
-//   }
-// ]
+export const filterBtnLabelForPortfolio = [
+  '전체', '문제해결 체험', '상상 더하기 체험', '웹툰 생성 체험', 'AI 간편 체험'
+]
+
+export const portfolioList = [
+  {
+    idx: 1,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-23', heartRate: 10,
+  },
+  {
+    idx: 2,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-22', heartRate: 10,
+  },
+  {
+    idx: 3,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-21', heartRate: 10,
+  },
+  {
+    idx: 4,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-20',
+    heartRate: 10,
+  },
+  {
+    idx: 5,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 10,
+  },
+  {
+    idx: 6,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 11,
+  },
+  {
+    idx: 7,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 12,
+  },
+  {
+    idx: 8,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 15,
+  },
+  {
+    idx: 9,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 20,
+  },
+  {
+    idx: 10,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 32,
+  },
+  {
+    idx: 11,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 1,
+  },
+  {
+    idx: 12,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 0,
+  },
+  {
+    idx: 13,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 10,
+  },
+  {
+    idx: 14,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 10,
+  },
+  {
+    idx: 15,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 10,
+  },
+  {
+    idx: 16,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 2,
+  },
+  {
+    idx: 17,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 11,
+  },
+  {
+    idx: 18,
+    imgUrl: 'https://placehold.co/150',
+    title: '강남구 미래인재 스마트시티',
+    subtitle: '안전한 나의 친구',
+    user: 'ahnji1',
+    timestamp: '2024-10-25',
+    heartRate: 2,
+  }
+]
+
 export const projectList: Project[] = [
   {
     idx: 1,
@@ -159,21 +246,21 @@ export const projectList: Project[] = [
   },
   {
     idx: 12,
-    title: '플래닛파크 문제해결 프로젝트',
+    title: '우주도시 문제해결 프로젝트',
     subTitle: 'eco friendly project',
     imgUrl: 'https://placehold.co/150',
     tag: '카드뉴스'
   },
   {
     idx: 13,
-    title: '플래닛파크 문제해결 프로젝트',
+    title: '플래닛공원 문제해결 프로젝트',
     subTitle: 'eco friendly project',
     imgUrl: 'https://placehold.co/150',
     tag: '카드뉴스'
   },
   {
     idx: 14,
-    title: '플래닛파크 문제해결 프로젝트',
+    title: '플래닛공원 문제해결 프로젝트',
     subTitle: 'eco friendly project',
     imgUrl: 'https://placehold.co/150',
     tag: '기본'
@@ -207,3 +294,5 @@ export const projectList: Project[] = [
     tag: '카드뉴스'
   },
 ]
+
+
