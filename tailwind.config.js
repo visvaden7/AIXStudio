267bb84ceae330ac309bcabeb881047ab66e6063
visvaden7 <visvaden7@naver.com>
2025-01-08 18:03:08 +0900

    chore: move file and setting config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
       './src/**/*.{html,js,jsx,ts,tsx}'
   ],
   theme: {
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '19200px',
+    },
     extend: {
       fontFamily: {
         sans: ['Pretendard', 'ui-sans-serif', 'system-ui'],
