c8f37347bd2d7dc2bb607c791997b7ea83dfb62b
visvaden7 <visvaden7@naver.com>
2025-01-16 18:17:09 +0900

    feat: typeGuard.ts
    - add typeGuard by portfolio, project and general

---
+++ src/utils/typeGuard.ts
@@ -0,0 +1,27 @@
+import {Portfolio, Project} from "../@types/domain.ts";
+
+const isOfType = <T>(value: unknown, keys: (keyof T)[]): value is T => {
+  if (typeof value !== 'object' || value === null) {
+    return false;
+  }
+  for (const key of keys) {
+    if (!(key in value)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+export const isProject = (value: unknown): value is Project => {
+  const keys:(keyof Project)[] = [
+    'idx', 'titleKo', 'titleEn', 'subTitle', 'imgUrl', 'hash', 'type', 'story', 'timeStamp',
+  ];
+  return isOfType<Project>(value, keys)
+}
+
+export const isPortfolio = (value: unknown): value is Portfolio => {
+  const keys:(keyof Portfolio)[] = [
+    'idx', "imgUrl", "title", "subtitle", "type", 'user', "timeStamp", 'heartRate'
+  ];
+  return isOfType<Portfolio>(value, keys)
+}
